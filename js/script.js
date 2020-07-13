c77a18ba3787356d4629861811058aee9f16a6bb
webcoding_by <webcoding.by@gmail.com>
2020-07-13 12:40:12 +0300

    task 1 + task 2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Task #1
+
+// let firstNumber = +prompt('Введите число ');
+// let subtractNumber = +prompt('Сколько отнять от вашего числа?');
+// let addNumber = +prompt('Сколько прибавить к результату?');
+// let multiplyNumber = +prompt('На сколько умножить результат?');
+// let divideNumber = +prompt('На сколько разделить результат?');
+// let totalNumber = (firstNumber - subtractNumber + addNumber) * multiplyNumber / divideNumber;
+
+// alert('(' + firstNumber + '-' + subtractNumber + '+' + addNumber + ')' + '*' + multiplyNumber + '/' + divideNumber + '=' + totalNumber);
+
+// Task #2
+
+let userName = "Vlasimir"; 
+let userAge = 31; 
+let userSkills = ['HTML',' Css', 'Js']; 
+let userNameType = typeof userName; // string
+let userAgeType = typeof userAge; // number
+let userSkillsType = typeof userSkills // object
+
+console.log('Переменная userName имеет тип -' + ' ' + userNameType);
+console.log('Переменная userAge имеет тип -' + ' ' + userAgeType);
+console.log('Переменная userSkills имеет тип -' + ' ' + userSkillsType);
