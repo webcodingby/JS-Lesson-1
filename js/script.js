8c00706696f4458f111549e44406228797d35bfe
webcoding_by <webcoding.by@gmail.com>
2020-07-13 12:46:17 +0300

    Task 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Task #1
 
-// let firstNumber = +prompt('Введите число ');
-// let subtractNumber = +prompt('Сколько отнять от вашего числа?');
-// let addNumber = +prompt('Сколько прибавить к результату?');
-// let multiplyNumber = +prompt('На сколько умножить результат?');
-// let divideNumber = +prompt('На сколько разделить результат?');
-// let totalNumber = (firstNumber - subtractNumber + addNumber) * multiplyNumber / divideNumber;
+let firstNumber = +prompt('Введите число ');
+let subtractNumber = +prompt('Сколько отнять от вашего числа?');
+let addNumber = +prompt('Сколько прибавить к результату?');
+let multiplyNumber = +prompt('На сколько умножить результат?');
+let divideNumber = +prompt('На сколько разделить результат?');
+let totalNumber = (firstNumber - subtractNumber + addNumber) * multiplyNumber / divideNumber;
 
-// alert('(' + firstNumber + '-' + subtractNumber + '+' + addNumber + ')' + '*' + multiplyNumber + '/' + divideNumber + '=' + totalNumber);
+alert('(' + firstNumber + '-' + subtractNumber + '+' + addNumber + ')' + '*' + multiplyNumber + '/' + divideNumber + '=' + totalNumber);
 
 // Task #2
 
@@ -21,3 +21,11 @@ let userSkillsType = typeof userSkills // object
 console.log('Переменная userName имеет тип -' + ' ' + userNameType);
 console.log('Переменная userAge имеет тип -' + ' ' + userAgeType);
 console.log('Переменная userSkills имеет тип -' + ' ' + userSkillsType);
+
+
+// Task 3
+
+let firstNumeric = 2;
+let secondNumeric = 4;
+
+console.log(firstNumeric.toString() + secondNumeric.toString());
